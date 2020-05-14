77b4d1c40e1136196aa962af8f57ed7a1668f15c
Ramon Audain <raudain123@gmail.com>
2020-05-14 08:32:08 -0700

    fix typo

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Given an array and chunk size, divid the array into many subarrays
+// Given an array and chunk size, divide the array into many subarrays
 // where each subarray is of length size
 
 function arrChunk(array, size){
